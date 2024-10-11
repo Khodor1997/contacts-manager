ed04c9f985a8058bdad836d4efef97cd6d1efb38
Pasha <pasha_khodor@mail.ru>
2024-10-11 15:37:13 +0300

    update contactCreateForm

---
+++ src/components/buttons/ButtonForm.jsx
@@ -0,0 +1,24 @@
+import StarIcon from '../icons/StarIcon';
+import styles from './styles.module.css'
+
+export default function ButtonForm ({ children, type = "button", onClick, onFavoriteClick, isFavorite}) {
+    return (
+        <div 
+            className={styles['button-form']}
+        >
+            <span style={{display: "flex"}} onClick={onFavoriteClick}>
+                <StarIcon 
+                    className={`${styles['button-form__icon']} ${isFavorite ? styles['button-form__favorite'] : ''}`}
+                />
+            </span>
+            <div></div>
+            <button
+                className={styles['button-form__text']}
+                type={type} 
+                onClick={onClick} 
+            >
+                {children}
+            </button>
+        </div>
+    )
+}
--- src/components/contacts/ContactCreateForm.jsx
+++ src/components/contacts/ContactCreateForm.jsx
@@ -1,4 +1,5 @@
 import InputField from '../inputs/InputField';
+import ButtonForm from '../buttons/ButtonForm';
 import { useState } from 'react';
 import styles from './style.module.css';
 import createImage from './../../assets/icons/add-image.svg';
@@ -16,18 +17,28 @@ export default function ContactCreateForm() {
     const [address, setAddress] = useState('');
     const [message, setMessage] = useState('');
 
+    const [isFavorite, setIsFavorite] = useState(false);
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        const formData = {
-            name,
-            email,
-            phone,
-            address,
-            message,
-        };
-        console.log(formData); // Логика отправки формы
+        const formData = { name, email, phone, address, message, isFavorite };
+
+        console.log(formData);
+        clearForm()
     };
 
+    const handleFavoriteClick = () => {
+        setIsFavorite(!isFavorite);
+        console.log('111111')
+    };
+
+    const clearForm = (() => {
+        setName('');
+        setEmail('');
+        setPhone('');
+        setAddress('');
+        setMessage('');
+    })
     return (
         <div className={styles['create-contact']}>
             <div className={styles['create-contact__image']}>
@@ -44,7 +55,14 @@ export default function ContactCreateForm() {
                     <InputField icon={mail} altText="Address icon" value={address} onChange={setAddress} placeholder="Enter your address" />
                     <InputField icon={edit} altText="Message icon" value={message} onChange={setMessage} placeholder="Enter your message" />
                     
-                    <button type="submit">Submit</button>
+                    {/* <button type="submit">Submit</button> */}
+                    <ButtonForm
+                        type="submit"
+                        isFavorite={isFavorite} 
+                        onFavoriteClick={handleFavoriteClick}
+                    >
+                        Submit
+                    </ButtonForm>
                 </form>
             </div>
         </div>
